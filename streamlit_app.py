823fb53f88001bdc021b30ceb5e9e9f1dd059123
Hugo Le Moine <39267118+hugolmn@users.noreply.github.com>
2022-06-12 18:38:48 +0200

    Loading data from ishares

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,14 @@
 import streamlit
+import pandas as pd
+import requests
 
 streamlit.title("ETF Composition")
+
+try:
+  response = requests.get('https://www.ishares.com/us/product-screener/product-screener-v3.1.jsn?dcrPath=/templatedata/config/product-screener-v3/data/en/us-ishares/ishares-product-screener-backend-config&siteEntryPassthrough=true').json()
+except:
+  streamlit.error()
+  
+df = pd.DataFrame(response).T
+
+streamlit.dataframe(df)
