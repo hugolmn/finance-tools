3a7a81c145379d5ef7717058fa778c3e34788d7f
Hugo Le Moine <hugo.le-moine@outlook.fr>
2022-06-23 19:15:20 +0200

    Multipage management

---
+++ pages/Fund Analyzer.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import pandas as pd
+import requests
+
+st.title("ETF Composition")
+
+df = pd.read_csv('data/fund_compositions.csv')
+
+choice = st.selectbox("Select a fund", df.Fund.unique())
+
+selected_fund = df[df.Fund == choice].drop(columns=['Fund']).copy()
+sectors = selected_fund.groupby('Sector')['Weight (%)'].sum()
+regions = selected_fund.groupby('Location')['Weight (%)'].sum()
+
+col1, col2, col3 = st.columns(3)
+col1.metric("Top 10 concentration", value=f"{selected_fund.iloc[:10]['Weight (%)'].sum():.0f}%")
+col2.metric("Largest sector", value=f'{sectors.max():.0f}%', delta=sectors.idxmax(), delta_color='off')
+col3.metric("Largest region", value=f'{regions.max():.0f}%', delta=regions.idxmax(), delta_color='off')
+
+st.header('Holdings')
+st.dataframe(selected_fund)
