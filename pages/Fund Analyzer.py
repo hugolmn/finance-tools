45e717406d3e11a7dcd82fa4c80e5e3bb098f807
Hugo Le Moine <hugo.le-moine@outlook.fr>
2022-06-23 19:21:32 +0200

    Added sector chart with altair

--- pages/Fund Analyzer.py
+++ pages/Fund Analyzer.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import requests
+import altair as alt
 
 st.title("ETF Composition")
 
@@ -16,6 +17,13 @@ col1, col2, col3 = st.columns(3)
 col1.metric("Top 10 concentration", value=f"{selected_fund.iloc[:10]['Weight (%)'].sum():.0f}%")
 col2.metric("Largest sector", value=f'{sectors.max():.0f}%', delta=sectors.idxmax(), delta_color='off')
 col3.metric("Largest region", value=f'{regions.max():.0f}%', delta=regions.idxmax(), delta_color='off')
+
+st.header('Sectors')
+c = alt.Chart(sectors.reset_index()).mark_bar().encode(
+    x='Weight (%)',
+    y='Sector'
+)
+st.altair_chart(c, use_container_width=True)
 
 st.header('Holdings')
 st.dataframe(selected_fund)
