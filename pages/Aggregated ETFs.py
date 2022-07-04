8bf3f91b6860885dcfef97b39bcc3a59ff441539
Hugo Le Moine <39267118+hugolmn@users.noreply.github.com>
2022-07-04 21:09:21 +0200

    Update Aggregated ETFs.py

--- pages/Aggregated ETFs.py
+++ pages/Aggregated ETFs.py
@@ -3,7 +3,7 @@ import pandas as pd
 import requests
 import altair as alt
 
-st.title("ETF Composition")
+st.title("Aggregate portfolio of ETFs")
 
 df = pd.read_csv('data/blackrock_fr.csv')
 
@@ -23,6 +23,39 @@ if clicked:
   portfolio = portfolio.groupby(['Ticker', 'Name', 'Sector', 'Asset Class', 'Location'], as_index=False).Value.sum()
   portfolio['Weight (%)'] = portfolio.Value.div(portfolio.Value.sum()) * 100                              
                               
+  sectors = portfolio.groupby('Sector')['Weight (%)'].sum()
+  regions = portfolio.groupby('Location')['Weight (%)'].sum()
+  asset_classes = portfolio.groupby('Asset Class')['Weight (%)'].sum()
+
+  col1, col2, col3 = st.columns(3)
+  col1.metric("Top 10 concentration", value=f"{selected_fund.iloc[:10]['Weight (%)'].sum():.0f}%")
+  col2.metric("Largest sector", value=f'{sectors.max():.0f}%', delta=sectors.idxmax(), delta_color='off')
+  col3.metric("Largest region", value=f'{regions.max():.0f}%', delta=regions.idxmax(), delta_color='off')
+
+  st.header('Sectors')
+  c_sectors = alt.Chart(sectors.sort_values().reset_index()).mark_bar().encode(
+      x='Weight (%)',
+      y=alt.Y('Sector', sort='-x'),
+      tooltip=['Sector', 'Weight (%)']
+  )
+  st.altair_chart(c_sectors, use_container_width=True)
+
+  st.header('Asset Classes')
+  c_asset_classes = alt.Chart(asset_classes.sort_values().reset_index()).mark_bar().encode(
+      x='Weight (%)',
+      y='Asset Class',
+      tooltip=['Asset Class', 'Weight (%)']
+  )
+  st.altair_chart(c_asset_classes, use_container_width=True)
+
+  st.header('Regions')
+  c = alt.Chart(regions.sort_values().reset_index()).mark_bar().encode(
+      x='Weight (%)',
+      y=alt.Y('Location', sort='-x'),
+      tooltip=['Location', 'Weight (%)']
+  )
+  st.altair_chart(c, use_container_width=True)
+  
   st.header('Holdings')
   st.dataframe(portfolio)
     
