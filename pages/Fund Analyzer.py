8b8d2ca68c7c3afb3c89d258e88082a081e06cf9
Hugo Le Moine <39267118+hugolmn@users.noreply.github.com>
2022-07-04 20:39:18 +0200

    Update Fund Analyzer.py

--- pages/Fund Analyzer.py
+++ pages/Fund Analyzer.py
@@ -21,23 +21,21 @@ col1.metric("Top 10 concentration", value=f"{selected_fund.iloc[:10]['Weight (%)
 col2.metric("Largest sector", value=f'{sectors.max():.0f}%', delta=sectors.idxmax(), delta_color='off')
 col3.metric("Largest region", value=f'{regions.max():.0f}%', delta=regions.idxmax(), delta_color='off')
 
-chart_sector, chart_asset_classes = st.columns(2)
-
-chart_sector.header('Sectors')
+st.header('Sectors')
 c_sectors = alt.Chart(sectors.sort_values().reset_index()).mark_bar().encode(
     x='Weight (%)',
     y=alt.Y('Sector', sort='-x'),
     tooltip=['Sector', 'Weight (%)']
 )
-chart_sector.altair_chart(c_sectors, use_container_width=True)
+st.altair_chart(c_sectors, use_container_width=True)
 
-chart_asset_classes.header('Asset Classes')
+st.header('Asset Classes')
 c_asset_classes = alt.Chart(asset_classes.sort_values().reset_index()).mark_bar().encode(
     x='Weight (%)',
     y='Asset Class',
     tooltip=['Asset Class', 'Weight (%)']
 )
-chart_asset_classes.altair_chart(c_asset_classes, use_container_width=True)
+st.altair_chart(c_asset_classes, use_container_width=True)
 
 st.header('Regions')
 c = alt.Chart(regions.sort_values().reset_index()).mark_bar().encode(
