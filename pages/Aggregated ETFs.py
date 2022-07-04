9a7c15f627f2191d65fd9adcc71940aa27a5fb94
Hugo Le Moine <39267118+hugolmn@users.noreply.github.com>
2022-07-04 21:07:53 +0200

    Update Aggregated ETFs.py

--- pages/Aggregated ETFs.py
+++ pages/Aggregated ETFs.py
@@ -20,7 +20,7 @@ if clicked:
     portfolio.loc[portfolio.Fund == etf, 'Value'] = portfolio.loc[portfolio.Fund == etf, 'Weight (%)'] * holding / 100
     
   portfolio = portfolio.drop(columns=['Fund'])
-  portfolio = portfolio.groupby(['Ticker', 'Name', 'Sector', 'Asset Class', 'Location']).Value.sum()
+  portfolio = portfolio.groupby(['Ticker', 'Name', 'Sector', 'Asset Class', 'Location'], as_index=False).Value.sum()
   portfolio['Weight (%)'] = portfolio.Value.div(portfolio.Value.sum()) * 100                              
                               
   st.header('Holdings')
