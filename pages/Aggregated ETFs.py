db336d641cbb130a0b522a3e35f72de10c46607b
Hugo Le Moine <39267118+hugolmn@users.noreply.github.com>
2022-07-04 21:02:27 +0200

    Show aggregate portfolio

--- pages/Aggregated ETFs.py
+++ pages/Aggregated ETFs.py
@@ -14,6 +14,11 @@ holdings = [st.number_input(f'Total value of {choice} holding', min_value=0, ste
 clicked = st.button('Show results')
 
 if clicked:
+  portfolio = df[df.Fund.isin(choices)]
   for etf, holding in zip(choices, holdings):
     st.text(f'{etf}: {holding}€')
+    portfolio.loc[portfolio.Fund == etf, 'Value'] = portfolio.loc[portfolio.Fund == etf, 'Weight (%)'] * holding / 100
+    
+  st.header('Holdings')
+  st.dataframe(selected_fund)
     
