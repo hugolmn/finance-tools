381a5e6bc598ce672624dad957409212c82eac5c
Hugo Le Moine <hugo.le-moine@outlook.fr>
2022-08-25 16:11:58 +0200

    Management of multiple portfolios

--- pages/ETF & Stock Portfolio.py
+++ pages/ETF & Stock Portfolio.py
@@ -5,121 +5,166 @@ import altair as alt
 
 st.set_page_config(layout="wide")
 
+# Handling of cookies
 @st.cache(allow_output_mutation=True)
 def get_manager():
     return stx.CookieManager()
 
 cookie_manager = get_manager()
 cookies = cookie_manager.get_all()
-# st.write(cookies)
+existing_portfolios = cookie_manager.get(cookie='portfolios')
 
 st.title("Aggregate portfolio of ETFs and stocks")
 
-# ETFs
+# Load ETFs
 etfs = pd.read_csv('data/blackrock_fr.csv')
-previous_etfs = cookie_manager.get(cookie='etf_holdings')
-
-if previous_etfs:
-  etf_choices = st.multiselect("Select a fund", etfs.Fund.unique(), default=previous_etfs.keys())
-else:
-  etf_choices = st.multiselect("Select a fund", etfs.Fund.unique())
-
-etf_holdings = {}
-for etf in etf_choices:
-  if previous_etfs and etf in previous_etfs:
-    etf_holdings[etf] = st.number_input(f'Total value of {etf} holding', min_value=0, step=10, value=previous_etfs[etf])
-  else:
-    etf_holdings[etf] = st.number_input(f'Total value of {etf} holding', min_value=0, step=10)
-
-# Stocks
+# Load Stocks
 stocks = pd.read_csv('data/individual_positions.csv')
-previous_stocks = cookie_manager.get(cookie='stock_holdings')
 
-if previous_stocks:
-  stock_choices = st.multiselect("Select a stock", stocks.Name.unique(), default=previous_stocks.keys())
+use_saved_portfolio = st.checkbox('Use saved portfolio')
+
+if use_saved_portfolio and (len(existing_portfolios) > 0):
+  selected_portfolio_name = st.selectbox('Select portfolio', existing_portfolios.keys(), disabled=not use_saved_portfolio)
+  portfolio_name = st.text_input('Portfolio name', value=selected_portfolio_name)
+  portfolio = existing_portfolios[selected_portfolio_name] 
 else:
-  stock_choices = st.multiselect("Select a stock", stocks.Name.unique())
+  portfolio_name = st.text_input('Portfolio name')
+  portfolio = {
+    'etf_holdings': {},
+    'stock_holdings': {}
+  }
 
-stock_holdings = {}
-for stock in stock_choices:
-  if previous_stocks and stock in previous_stocks:
-    stock_holdings[stock] = st.number_input(f'Total value of {stock} holding', min_value=0, step=10, value=previous_stocks[stock])
-  else:
-    stock_holdings[stock] = st.number_input(f'Total value of {stock} holding', min_value=0, step=10)
+# Portfolio ETFs
+with st.expander(f"ETF holdings"):
+  etf_choices = st.multiselect(
+    "Select ETFs",
+    options=etfs.Fund.unique(),
+    default=portfolio['etf_holdings'] if portfolio['etf_holdings'] else None
+  )
 
-clicked = st.button('Show results')
-saved = st.button('Save holdings')
+  portfolio['etf_holdings'] = {
+    etf: portfolio['etf_holdings'][etf]
+    for etf in portfolio['etf_holdings']
+    if etf in etf_choices
+  }
+
+  for etf in etf_choices:
+    default_value = portfolio['etf_holdings'][etf] if etf in portfolio['etf_holdings'] else 0
+    portfolio['etf_holdings'][etf] = st.number_input(
+      f'Total value of {etf} holding',
+      min_value=0,
+      step=100,
+      value=default_value
+    )
+
+# Portfolio stocks
+with st.expander(f"Stock holdings"):
+  stock_choices = st.multiselect(
+    "Select stocks",
+    options=stocks.Name.unique(),
+    default=portfolio['stock_holdings'] if portfolio['stock_holdings'] else None
+  )
+
+  portfolio['stock_holdings'] = {
+    stock: portfolio['stock_holdings'][stock]
+    for stock in portfolio['stock_holdings']
+    if stock in stock_choices
+  }
+
+  for stock in stock_choices:
+    default_value = portfolio['stock_holdings'][stock] if stock in portfolio['stock_holdings'] else 0
+    portfolio['stock_holdings'][stock] = st.number_input(
+      f'Total value of {stock} holding',
+      min_value=0,
+      step=100,
+      value=default_value
+    )
+
+
+# Action buttons
+col1, col2, col3 = st.columns(3)
+with col1:
+  clicked = st.button('Show portfolio',  disabled=len(portfolio_name) == 0)
+with col2: 
+  saved = st.button('Save portfolio', disabled=len(portfolio_name) == 0)
+with col3: 
+  delete = st.button('Delete portfolio', disabled=len(portfolio_name) == 0)
 
 if clicked:
-  portfolio = etfs[etfs.Fund.isin(etf_choices)].copy()
-  # for etf, holding in zip(etf_choices, etf_holdings):
-  for etf, holding in etf_holdings.items():
-    st.text(f'{etf}: {holding}€')
-    portfolio.loc[portfolio.Fund == etf, 'Value'] = portfolio.loc[portfolio.Fund == etf, 'Weight (%)'] * holding / 100
+  try:
+    etf_holdings = etfs[etfs.Fund.isin(etf_choices)].copy()
+    for etf, holding in portfolio['etf_holdings'].items():
+      st.text(f'{etf}: {holding}€')
+      etf_holdings.loc[etf_holdings.Fund == etf, 'Value'] = etf_holdings.loc[etf_holdings.Fund == etf, 'Weight (%)'] * holding / 100
+      
+    stock_holdings = stocks[stocks.Name.isin(stock_choices)].copy()
+    for stock, holding in portfolio['stock_holdings'].items():
+      st.text(f'{stock}: {holding}€')
+      stock_holdings.loc[stock_holdings.Name == stock, 'Value'] = holding
     
-  stock_portfolio = stocks[stocks.Name.isin(stock_choices)]
-  # for stock, holding in zip(stock_choices, stock_holdings):
-  for stock, holding in stock_holdings.items():
-    st.text(f'{stock}: {holding}€')
-    stock_portfolio.loc[stock_portfolio.Name == stock, 'Value'] = holding
-  
-  portfolio = pd.concat([portfolio, stock_portfolio])
-  portfolio = portfolio.drop(columns=['Fund'])
-  portfolio = portfolio.groupby(['Ticker', 'Name', 'Sector', 'Asset Class', 'Location'], as_index=False).Value.sum()
-  portfolio['Weight (%)'] = portfolio.Value.div(portfolio.Value.sum()) * 100                              
-  portfolio['Weight (%)'] = portfolio['Weight (%)'].round(2)
-  portfolio = portfolio.sort_values(by='Value', ascending=False)
-  
-  sectors = portfolio.groupby('Sector')[['Value', 'Weight (%)']].sum()
-  regions = portfolio.groupby('Location')[['Value', 'Weight (%)']].sum()
-  asset_classes = portfolio.groupby('Asset Class')[['Value', 'Weight (%)']].sum()
+    holdings = pd.concat([etf_holdings, stock_holdings])
+    holdings = holdings.drop(columns=['Fund'])
+    holdings = holdings.groupby(['Ticker', 'Name', 'Sector', 'Asset Class', 'Location'], as_index=False).Value.sum()
+    holdings['Weight (%)'] = holdings.Value.div(holdings.Value.sum()) * 100                              
+    holdings['Weight (%)'] = holdings['Weight (%)'].round(2)
+    holdings = holdings.sort_values(by='Value', ascending=False)
+    
+    sectors = holdings.groupby('Sector')[['Value', 'Weight (%)']].sum()
+    regions = holdings.groupby('Location')[['Value', 'Weight (%)']].sum()
+    asset_classes = holdings.groupby('Asset Class')[['Value', 'Weight (%)']].sum()
 
-  col1, col2, col3 = st.columns(3)
-  col1.metric(
-    "Top 10 concentration",
-    value=f"{portfolio.iloc[:10]['Weight (%)'].sum():.0f}%"
-  )
-  col2.metric(
-    "Largest sector",
-    value=f"{sectors['Weight (%)'].max():.0f}%",
-    delta=sectors['Weight (%)'].idxmax(),
-    delta_color='off'
-  )
-  col3.metric(
-    "Largest region", 
-    value=f"{regions['Weight (%)'].max():.0f}%",
-    delta=regions['Weight (%)'].idxmax(),
-    delta_color='off'
-  )
+    col1, col2, col3 = st.columns(3)
+    col1.metric(
+      "Top 10 concentration",
+      value=f"{holdings.iloc[:10]['Weight (%)'].sum():.0f}%"
+    )
+    col2.metric(
+      "Largest sector",
+      value=f"{sectors['Weight (%)'].max():.0f}%",
+      delta=sectors['Weight (%)'].idxmax(),
+      delta_color='off'
+    )
+    col3.metric(
+      "Largest region", 
+      value=f"{regions['Weight (%)'].max():.0f}%",
+      delta=regions['Weight (%)'].idxmax(),
+      delta_color='off'
+    )
 
-  st.header('Sectors')
-  c_sectors = alt.Chart(sectors.sort_values(by='Value').reset_index()).mark_bar().encode(
-      x='Weight (%)',
-      y=alt.Y('Sector', sort='-x'),
-      tooltip=['Sector', 'Weight (%)', 'Value']
-  )
-  st.altair_chart(c_sectors, use_container_width=True)
+    st.header('Sectors')
+    c_sectors = alt.Chart(sectors.sort_values(by='Value').reset_index()).mark_bar().encode(
+        x='Weight (%)',
+        y=alt.Y('Sector', sort='-x'),
+        tooltip=['Sector', 'Weight (%)', 'Value']
+    )
+    st.altair_chart(c_sectors, use_container_width=True)
 
-  st.header('Asset Classes')
-  c_asset_classes = alt.Chart(asset_classes.sort_values(by='Value').reset_index()).mark_bar().encode(
-      x='Weight (%)',
-      y='Asset Class',
-      tooltip=['Asset Class', 'Weight (%)', 'Value']
-  )
-  st.altair_chart(c_asset_classes, use_container_width=True)
+    st.header('Asset Classes')
+    c_asset_classes = alt.Chart(asset_classes.sort_values(by='Value').reset_index()).mark_bar().encode(
+        x='Weight (%)',
+        y='Asset Class',
+        tooltip=['Asset Class', 'Weight (%)', 'Value']
+    )
+    st.altair_chart(c_asset_classes, use_container_width=True)
 
-  st.header('Regions')
-  c = alt.Chart(regions.sort_values(by='Value').reset_index()).mark_bar().encode(
-      x='Weight (%)',
-      y=alt.Y('Location', sort='-x'),
-      tooltip=['Location', 'Weight (%)', 'Value']
-  )
-  st.altair_chart(c, use_container_width=True)
-  
-  st.header('Holdings')
-  st.dataframe(portfolio)
+    st.header('Regions')
+    c = alt.Chart(regions.sort_values(by='Value').reset_index()).mark_bar().encode(
+        x='Weight (%)',
+        y=alt.Y('Location', sort='-x'),
+        tooltip=['Location', 'Weight (%)', 'Value']
+    )
+    st.altair_chart(c, use_container_width=True)
+    
+    st.header('Holdings')
+    st.dataframe(holdings.reset_index(drop=True))
+  except:
+    st.error('No position found in portfolio')
 
 if saved:
-  cookie_manager.set('etf_holdings', etf_holdings, key='etf_hokdings')
-  cookie_manager.set('stock_holdings', stock_holdings, key='stock_holdings')
+  existing_portfolios[portfolio_name] = portfolio
+  cookie_manager.set('portfolios', existing_portfolios, key='existing_portfolios')
+
+if delete:
+  del(existing_portfolios[portfolio_name])
+  cookie_manager.set('portfolios', existing_portfolios, key='existing_portfolios')
     
