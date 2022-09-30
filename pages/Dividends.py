5bb8e7c2e04507446a0496f278a2bc680c9be38b
Hugo Le Moine <hugo.le-moine@outlook.fr>
2022-09-30 20:45:21 +0200

    Use rolling median, metric for averate yield

--- pages/Dividends.py
+++ pages/Dividends.py
@@ -10,10 +10,9 @@ st.title('Dividends')
 col1, col2 = st.columns(2)
 ticker = col1.text_input("Ticker", value='MSFT')
 period = col2.selectbox("Periodd", options=[
-        '1mo', '3mo', '6mo', 'ytd', '1y', '2y',
         '5y', '10y', '15y', '20y', '25y', 'max'
     ],
-    index=6
+    index=2
 )
 
 if ticker:
@@ -24,41 +23,26 @@ if ticker:
     # Number of distributions per year
     dist_per_year = dividends.Dividends.resample('365d').count().mode()[0]
 
-    # Rolling sum of dividends with smoothing to ignore special dividends.
+    # Rolling median of Dividends over 5 distributions to ignore special dividends
     # Might not be 100% accurate
-    dividends['SmoothedYearlyDiv'] = (dividends
+    dividends['SmoothedDividends'] = (dividends
         .Dividends
-        .rolling('365d')
-        .apply(lambda x: pd.Series.mode(x)[0])
-        .rolling('350d', min_periods=dist_per_year-1)
-        .sum()
+        .rolling(5, center=True)
+        .median()
     )
+
+    dividends = dividends.loc[dividends.SmoothedDividends.first_valid_index():]
+    dividends['SmoothedDividends'] = dividends.SmoothedDividends.combine_first(dividends.Dividends)
+    dividends['YearlyDividends'] = dividends.SmoothedDividends * dist_per_year
+
     # Growth in dividends since beginning of timeframe
-    dividends['DivGrowth'] = dividends['Dividends'] / dividends['Dividends'].iloc[0] - 1
+    dividends['DivGrowth'] = dividends['SmoothedDividends'] / dividends['SmoothedDividends'].iloc[0] - 1
     dividends = dividends.reset_index()
 
     # Number of years between first and last dividend in timeframe
     n_years = (dividends.iloc[-1].Date - dividends.iloc[0].Date) / datetime.timedelta(365.2425)
     # CAGR of dividend
     div_cagr = (1 + dividends.iloc[-1].DivGrowth) ** (1 / n_years) - 1
-
-    col1, col2, col3, col4 = st.columns(4)
-    col1.metric(
-        label=f"Last 12m dividend",
-        value=f"${dividends.SmoothedYearlyDiv.iloc[-1]:,.2f}"
-    )
-    col2.metric(
-        label=f"Current Yield",
-        value=f"{dividends.SmoothedYearlyDiv.iloc[-1] / history.iloc[-1].Close:,.2%}"
-    )
-    col3.metric(
-        label=f"Average Yield",
-        value=f"TBD"
-    )
-    col4.metric(
-        label=f"{period} Dividend CAGR",
-        value=f"{div_cagr:+,.1%}"
-    )
 
     df = pd.merge(
         left=history.reset_index(),
@@ -67,14 +51,34 @@ if ticker:
         how='left'
     ).ffill()
 
+    df['DividendYieldTTM'] = df.YearlyDividends / df.Close
+
+    col1, col2, col3, col4 = st.columns(4)
+    col1.metric(
+        label=f"Last 12m dividend",
+        value=f"${df.YearlyDividends.iloc[-1]:,.2f}"
+    )
+    col2.metric(
+        label=f"Current Yield",
+        value=f"{df.DividendYieldTTM.iloc[-1]:,.2%}"
+    )
+    col3.metric(
+        label=f"{period} Average Yield",
+        value=f"{df.DividendYieldTTM.dropna().mean():,.2%}"
+    )
+    col4.metric(
+        label=f"{period} Dividend CAGR",
+        value=f"{div_cagr:+,.1%}"
+    )
+
     base = alt.Chart(df).mark_line(interpolate='step-after').encode(
         x=alt.X('Date:T')
     )
 
     dividend_chart = base.encode(
         y=alt.Y(
-            'SmoothedYearlyDiv:Q',
-            title='TTM dividends',
+            'YearlyDividends:Q',
+            title='Yearly Dividends',
             axis=alt.Axis(format='$.2f'),
             scale=alt.Scale(zero=False)
         ),
