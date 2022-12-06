c8a2978d9780b67eb206598e378352f6835801a8
Hugo Le Moine <hugo.le-moine@outlook.fr>
2022-12-06 09:17:42 +0100

    Added chart like twitter bot

--- pages/Dividends.py
+++ pages/Dividends.py
@@ -8,6 +8,8 @@ import datetime
 import seaborn as sns
 from utils import load_css
 
+st.set_page_config(layout="wide")
+
 def load_ticker_data(ticker, period):
     return yf.Ticker(ticker).history(
         period=period,
@@ -78,148 +80,156 @@ period = col2.selectbox("Periodd", options=[
 )
 
 if ticker:
-    # Load historical data
-    history = load_ticker_data(
-        ticker=ticker,
-        period=f"{int(period.split('y')[0]) + 1}y" if 'y' in period else period
-    )
+    from utils import generate_dividend_chart
+    price_chart, yield_chart, drawdown_chart = generate_dividend_chart(ticker, period)
+    st.altair_chart(price_chart, use_container_width=True, theme='streamlit')
+    st.altair_chart(yield_chart, use_container_width=True, theme='streamlit')
+    st.altair_chart(drawdown_chart, use_container_width=True, theme='streamlit')
 
-    dividends = process_dividend_history(history)
+# if ticker:
+#     # Load historical data
+#     history = load_ticker_data(
+#         ticker=ticker,
+#         period=f"{int(period.split('y')[0]) + 1}y" if 'y' in period else period
+#     )
 
-    # Number of years between first and last dividend in timeframe
-    n_years = (dividends.iloc[-1].Date - dividends.iloc[0].Date) / datetime.timedelta(365.2425)
-    # CAGR of dividend
-    div_cagr = (1 + dividends.iloc[-1].DivGrowth) ** (1 / n_years) - 1
+#     dividends = process_dividend_history(history)
 
-    # Merge dividends with price history
-    df = pd.merge(
-        left=history.reset_index(),
-        right=dividends.drop(columns=['Dividends']),
-        on='Date',
-        how='left'
-    ).ffill(limit=300).fillna(0)
+#     # Number of years between first and last dividend in timeframe
+#     n_years = (dividends.iloc[-1].Date - dividends.iloc[0].Date) / datetime.timedelta(365.2425)
+#     # CAGR of dividend
+#     div_cagr = (1 + dividends.iloc[-1].DivGrowth) ** (1 / n_years) - 1
 
-    index_first_dividend = df[df.YearlyDividends > 0].index[0]
-    df = df.loc[index_first_dividend:]
+#     # Merge dividends with price history
+#     df = pd.merge(
+#         left=history.reset_index(),
+#         right=dividends.drop(columns=['Dividends']),
+#         on='Date',
+#         how='left'
+#     ).ffill(limit=300).fillna(0)
 
-    # Drop where dividends is null
-    df = df[df.YearlyDividends.notna()]
-    # Calculate dividend yield base on TTM distributions
-    df['DividendYield'] = df.YearlyDividends / df.Close
+#     index_first_dividend = df[df.YearlyDividends > 0].index[0]
+#     df = df.loc[index_first_dividend:]
 
-    col1, col2, col3, col4 = st.columns(4)
-    col1.metric(
-        label=f"Annual dividends",
-        value=f"${df.YearlyDividends.iloc[-1]:,.2f}"
-    )
-    col2.metric(
-        label=f"Current Yield",
-        value=f"{df.DividendYield.iloc[-1]:,.2%}"
-    )
-    col3.metric(
-        label=f"{period} Median Yield",
-        value=f"{df.DividendYield.dropna().median():,.2%}"
-    )
-    col4.metric(
-        label=f"{period} Dividend CAGR",
-        value=f"{div_cagr:+,.1%}"
-    )
+#     # Drop where dividends is null
+#     df = df[df.YearlyDividends.notna()]
+#     # Calculate dividend yield base on TTM distributions
+#     df['DividendYield'] = df.YearlyDividends / df.Close
 
-    base = alt.Chart(df).mark_line(interpolate='step-after').encode(
-        x=alt.X('Date:T')
-    )
+#     col1, col2, col3, col4 = st.columns(4)
+#     col1.metric(
+#         label=f"Annual dividends",
+#         value=f"${df.YearlyDividends.iloc[-1]:,.2f}"
+#     )
+#     col2.metric(
+#         label=f"Current Yield",
+#         value=f"{df.DividendYield.iloc[-1]:,.2%}"
+#     )
+#     col3.metric(
+#         label=f"{period} Median Yield",
+#         value=f"{df.DividendYield.dropna().median():,.2%}"
+#     )
+#     col4.metric(
+#         label=f"{period} Dividend CAGR",
+#         value=f"{div_cagr:+,.1%}"
+#     )
 
-    dividend_chart = base.encode(
-        y=alt.Y(
-            'YearlyDividends:Q',
-            title='Yearly Dividends',
-            axis=alt.Axis(format='$.2f'),
-            scale=alt.Scale(zero=False)
-        ),
-        color=alt.value(st.secrets["theme"]['primaryColor'])
-    )
+#     base = alt.Chart(df).mark_line(interpolate='step-after').encode(
+#         x=alt.X('Date:T')
+#     )
 
-    price_chart = base.encode(
-        y=alt.Y(
-            'Close:Q',
-            title='Stock Price',
-            axis=alt.Axis(format='$.2f'),
-            scale=alt.Scale(zero=False)
-        ),
-        color=alt.value(st.secrets["theme"]['secondaryColor'])
-    )
+#     dividend_chart = base.encode(
+#         y=alt.Y(
+#             'YearlyDividends:Q',
+#             title='Yearly Dividends',
+#             axis=alt.Axis(format='$.2f'),
+#             scale=alt.Scale(zero=False)
+#         ),
+#         color=alt.value(st.secrets["theme"]['primaryColor'])
+#     )
 
-    chart = alt.layer(dividend_chart, price_chart).resolve_scale(
-        y = 'independent'
-    ).interactive(bind_y=False)
+#     price_chart = base.encode(
+#         y=alt.Y(
+#             'Close:Q',
+#             title='Stock Price',
+#             axis=alt.Axis(format='$.2f'),
+#             scale=alt.Scale(zero=False)
+#         ),
+#         color=alt.value(st.secrets["theme"]['secondaryColor'])
+#     )
 
-    st.altair_chart(chart, use_container_width=True)
+#     chart = alt.layer(dividend_chart, price_chart).resolve_scale(
+#         y = 'independent'
+#     ).interactive(bind_y=False)
 
-    # Calculate quantiles of dividend yield
-    quantiles = df.DividendYield.quantile(q=np.arange(0, 1.1, .1))
-    yield_df = pd.DataFrame(df.YearlyDividends.to_numpy()[:, None] / quantiles.to_numpy(), index=df.Date)
-    yield_df.columns = [f"Top {decile * 10}%" for decile in yield_df.columns[::-1]]
-    yield_df = yield_df.reset_index()
+#     st.altair_chart(chart, use_container_width=True)
 
-    # Create color palette and scale for legend
-    palette = sns.color_palette("vlag_r", len(quantiles)-1).as_hex()
-    scale = alt.Scale(domain=yield_df.columns[1:-1].tolist(), range=palette)
+#     # Calculate quantiles of dividend yield
+#     quantiles = df.DividendYield.quantile(q=np.arange(0, 1.1, .1))
+#     yield_df = pd.DataFrame(df.YearlyDividends.to_numpy()[:, None] / quantiles.to_numpy(), index=df.Date)
+#     yield_df.columns = [f"Top {decile * 10}%" for decile in yield_df.columns[::-1]]
+#     yield_df = yield_df.reset_index()
 
-    st.header(f"{ticker} {period} Yield Percentile Chart")
-    st.write(f"Current Yield: {df.iloc[-1].DividendYield:.2%} \
-        (Top {1 - df.DividendYield.rank(pct=True).iloc[-1]:.0%})")
+#     # Create color palette and scale for legend
+#     palette = sns.color_palette("vlag_r", len(quantiles)-1).as_hex()
+#     scale = alt.Scale(domain=yield_df.columns[1:-1].tolist(), range=palette)
 
-    # Create layers for chart
-    def make_layer(yield_df, col1, col2):
-        return alt.Chart(yield_df.assign(color=col1)).mark_area().encode(
-            x=alt.X('Date:T', title='', axis=alt.Axis(format='%Y')),
-        ).encode(
-            y=alt.Y(
-                f"{col1}:Q",
-                title='Stock Price',
-                axis=alt.Axis(format='$.0f'),
-                scale=alt.Scale(zero=False)
-            ),
-            y2=alt.Y2(
-                f"{col2}:Q",
-                title='Stock Price'
-            ),
-            color=alt.Color(
-                f"color:N",
-                title='Yield',
-                scale=scale,
-                legend=alt.Legend(
-                    titleFontSize=15,
-                    labelFontSize=10,
-                    titleLimit=0
-                )
-            ),
-            opacity=alt.value(0.8)
-        )
+#     st.header(f"{ticker} {period} Yield Percentile Chart")
+#     st.write(f"Current Yield: {df.iloc[-1].DividendYield:.2%} \
+#         (Top {1 - df.DividendYield.rank(pct=True).iloc[-1]:.0%})")
 
-    layers=[]
-    for col1, col2 in zip(yield_df.columns[1:-1], yield_df.columns[2:]):
-        layers.append(make_layer(yield_df, col1, col2))
+#     # Create layers for chart
+#     def make_layer(yield_df, col1, col2):
+#         return alt.Chart(yield_df.assign(color=col1)).mark_area().encode(
+#             x=alt.X('Date:T', title='', axis=alt.Axis(format='%Y')),
+#         ).encode(
+#             y=alt.Y(
+#                 f"{col1}:Q",
+#                 title='Stock Price',
+#                 axis=alt.Axis(format='$.0f'),
+#                 scale=alt.Scale(zero=False)
+#             ),
+#             y2=alt.Y2(
+#                 f"{col2}:Q",
+#                 title='Stock Price'
+#             ),
+#             color=alt.Color(
+#                 f"color:N",
+#                 title='Yield',
+#                 scale=scale,
+#                 legend=alt.Legend(
+#                     titleFontSize=15,
+#                     labelFontSize=10,
+#                     titleLimit=0
+#                 )
+#             ),
+#             opacity=alt.value(0.8)
+#         )
 
-    price = alt.Chart(df).mark_line(color='black').encode(
-        x='Date:T',
-        y='Close:Q'
-    )
-    layers.append(price)
+#     layers=[]
+#     for col1, col2 in zip(yield_df.columns[1:-1], yield_df.columns[2:]):
+#         layers.append(make_layer(yield_df, col1, col2))
+
+#     price = alt.Chart(df).mark_line(color='black').encode(
+#         x='Date:T',
+#         y='Close:Q'
+#     )
+#     layers.append(price)
     
-    chart = alt.layer(
-            *layers
-        ).configure(
-            font='Lato'
-        ).configure_axisY(
-            labelFontSize=15,
-            titleFontSize=10
-        ).configure_axisX(
-            labelAngle=-45,
-            labelFontSize=15
-        )
+#     chart = alt.layer(
+#             *layers
+#         ).configure(
+#             font='Lato'
+#         ).configure_axisY(
+#             labelFontSize=15,
+#             titleFontSize=10
+#         ).configure_axisX(
+#             labelAngle=-45,
+#             labelFontSize=15
+#         )
 
-    st.altair_chart(
-        chart.interactive(bind_y=False),
-        use_container_width=True
-    )
+#     st.altair_chart(
+#         chart.interactive(bind_y=False),
+#         use_container_width=True
+#     )
+
