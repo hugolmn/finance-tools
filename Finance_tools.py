e2cf59e119e08e03631ed30de123fdd0c98162af
Hugo Le Moine <hugo.le-moine@outlook.fr>
2024-08-07 17:18:01 +0200

    Removed pages no longer in use from main page

--- Finance_tools.py
+++ Finance_tools.py
@@ -8,11 +8,17 @@ st.write("""
     This streamlit app was built to host some personnal tools I need when it comes to investing.
     It's free to use and will always be."""
 )
+# st.write("""
+# ## Available pages:
+# - <a href='Dividends' target='_self'>Dividends</a>: Current and historical diviend yields of stocks.
+# - <a href='ETF_&_Stock_Portfolio' target='_self'>ETF & Stock_Portfolio</a>: Analyze a portfolio's actual holdings when it combines individual positions and ETFs.
+# - <a href='ETF_Analyzer' target='_self'>ETF_Analyzer</a>: Analyze holdings of BlackRock's ETFs by sector, asset class and regions.
+# - <a href='Total_Return' target='_self'>Total Return</a>: Visualize price and total returns, drawdown of stocks.
+# """, unsafe_allow_html=True)
+
 st.write("""
 ## Available pages:
 - <a href='Dividends' target='_self'>Dividends</a>: Current and historical diviend yields of stocks.
-- <a href='ETF_&_Stock_Portfolio' target='_self'>ETF & Stock_Portfolio</a>: Analyze a portfolio's actual holdings when it combines individual positions and ETFs.
-- <a href='ETF_Analyzer' target='_self'>ETF_Analyzer</a>: Analyze holdings of BlackRock's ETFs by sector, asset class and regions.
 - <a href='Total_Return' target='_self'>Total Return</a>: Visualize price and total returns, drawdown of stocks.
 """, unsafe_allow_html=True)
 
