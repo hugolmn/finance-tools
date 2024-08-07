3ff52b629583677dc4632c394bef0291eaeb620a
Hugo Le Moine <hugo.le-moine@outlook.fr>
2024-08-07 17:12:43 +0200

    Fixed periods options

--- pages/Total Return.py
+++ pages/Total Return.py
@@ -23,8 +23,7 @@ st.title('Total return calculator')
 col1, col2, col3 = st.columns(3)
 ticker = col1.text_input("Ticker", value='MSFT')
 period = col2.selectbox("Period", options=[
-        '1mo', '3mo', '6mo', 'ytd', '1y', '2y',
-        '5y', '10y', '15y', '20y', '25y', 'max'
+        '1d', '5d', '1mo', '3mo', '6mo', '1y', '2y', '5y', '10y', 'ytd', 'max'
     ],
     index=6
 )
